41d99594b597e525bcb160b9879eed8d3b632da9
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2020-03-19 18:13:33 +0300

    Add project files.

---
+++ AzureFunctionsRESTAPITableStorage/QueueListeners.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace AzureFunctionsRESTAPITableStorage
+{
+    public static class QueueListeners
+    {
+        [FunctionName("QueueListeners")]
+        public static async Task Run([QueueTrigger("todos", Connection = "AzureWebJobsStorage")]Todo todo,
+            [Blob("todos", Connection = "AzureWebJobsStorage")]CloudBlobContainer container,
+            ILogger log)
+        {
+            await container.CreateIfNotExistsAsync();
+            var blob = container.GetBlockBlobReference($"{todo.Id}.txt");
+            await blob.UploadTextAsync($"Created a new task: {todo.TaskDescription}");
+            log.LogInformation($"C# Queue trigger function processed: {todo.TaskDescription}");
+        }
+    }
+}
